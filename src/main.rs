4519c8afcdbea01608abd6f0f9ff8f572c606339
NULL-header <poaoq.poaoq@gmail.com>
2023-02-11 04:49:16 +0900

    feat: spawn

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,41 @@
+use bevy::prelude::*;
+
+fn setup(mut commands: Commands, asset_server: Res<AssetServer>) {
+    commands.spawn(Camera3dBundle::default());
+
+    let font: Handle<Font> = asset_server.load("Roboto-Regular.ttf");
+    commands
+        .spawn(NodeBundle {
+            style: Style {
+                align_items: AlignItems::Center,
+                ..default()
+            },
+            ..default()
+        })
+        .with_children(|parent| {
+            parent.spawn(
+                TextBundle::from_section(
+                    "Text",
+                    TextStyle {
+                        font,
+                        font_size: 50.,
+                        ..default()
+                    },
+                )
+                .with_style(Style {
+                    position: UiRect {
+                        left: Val::Percent(10.),
+                        ..default()
+                    },
+                    ..default()
+                }),
+            );
+        });
+}
+
 fn main() {
-    println!("Hello, world!");
+    App::new()
+        .add_plugins(DefaultPlugins)
+        .add_startup_system(setup)
+        .run();
 }
